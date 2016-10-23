18690930a2248b96dce392aff6b00dbd8d124d9b
Joel Nises <joel@nises.se>
2016-10-23 18:02:01 +0200

    more stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,30 @@
 #include <openvdb/openvdb.h>
+#include <openvdb/tools/LevelSetSphere.h>
+#include <openvdb/tools/VolumeToMesh.h>
+#include <vector>
 #include <iostream>
+#include <fstream>
 
-int main(int argc, const char *argv[])
+
+
+int main()
 {
-    // Initialize the OpenVDB library.  This must be called at least
-    // once per program and may safely be called multiple times.
     openvdb::initialize();
-    // Create an empty floating-point grid with background value 0.
-    openvdb::FloatGrid::Ptr grid = openvdb::FloatGrid::create();
-    std::cout << "Testing random access:" << std::endl;
-    // Get an accessor for coordinate-based access to voxels.
-    openvdb::FloatGrid::Accessor accessor = grid->getAccessor();
-    // Define a coordinate with large signed indices.
-    openvdb::Coord xyz(1000, -200000000, 30000000);
-
-    // Set the voxel value at (1000, -200000000, 30000000) to 1.
-    accessor.setValue(xyz, 1.0);
-
-    // Verify that the voxel value at (1000, -200000000, 30000000) is 1.
-    std::cout << "Grid" << xyz << " = " << accessor.getValue(xyz) << std::endl;
-
-    // Reset the coordinates to those of a different voxel.
-    xyz.reset(1000, 200000000, -30000000);
-
-    // Verify that the voxel value at (1000, 200000000, -30000000) is
-    // the background value, 0.
-    std::cout << "Grid" << xyz << " = " << accessor.getValue(xyz) << std::endl;
-
-    // Set the voxel value at (1000, 200000000, -30000000) to 2.
-    accessor.setValue(xyz, 2.0);
-    // Set the voxels at the two extremes of the available coordinate space.
-    // For 32-bit signed coordinates these are (-2147483648, -2147483648, -2147483648)
-    // and (2147483647, 2147483647, 2147483647).
-    accessor.setValue(openvdb::Coord::min(), 3.0f);
-    accessor.setValue(openvdb::Coord::max(), 4.0f);
-    std::cout << "Testing sequential access:" << std::endl;
-    // Print all active ("on") voxels by means of an iterator.
-    for (openvdb::FloatGrid::ValueOnCIter iter = grid->cbeginValueOn(); iter; ++iter) {
-        std::cout << "Grid" << iter.getCoord() << " = " << *iter << std::endl;
-    }
+    // Create a FloatGrid and populate it with a narrow-band
+    // signed distance field of a sphere.
+    openvdb::FloatGrid::Ptr grid =
+    openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(
+                                                             /*radius=*/50.0, /*center=*/openvdb::Vec3f(1.5, 2, 3),
+                                                             /*voxel size=*/0.5, /*width=*/4.0);
+    std::vector<openvdb::Vec3s> points;
+    std::vector<openvdb::Vec3I> tris;
+    std::vector<openvdb::Vec4I> quads;
+    openvdb::tools::volumeToMesh(*grid, points, tris, quads);
+    std::ofstream out("out.obj");
+    for(auto point: points)
+        out << "v " << point[0] << " " << point[1] << " " << point[2] << "\n";
+    for(auto tri: tris)
+        out << "f " << tri[0] + 1 << " " << tri[1] + 1 << " " << tri[2] + 1 << "\n";
+    for(auto quad: quads)
+        out << "f " << quad[0] + 1 << " " << quad[1] + 1 << " " << quad[2] + 1 << "\n";
 }
