fa9404fdc14fd9f7eab9b0a8bf153bc023653ff9
Joel Nises <joel@nises.se>
2016-10-24 21:50:25 +0200

    added union code

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <openvdb/tools/LevelSetSphere.h>
 #include <openvdb/tools/VolumeToMesh.h>
 #include <openvdb/tools/MeshToVolume.h>
+#include <openvdb/tools/Composite.h>
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -143,24 +144,39 @@ namespace
             return out;
         }
     };
+    
+    openvdb::FloatGrid::Ptr loadMesh(std::istream &infile)
+    {
+        assert(infile);
+        obj inobj;
+        infile >> inobj;
+        std::vector<openvdb::Vec3I> triangles;
+        for(auto const &face: inobj.faces)
+        {
+            assert(face.size() == 3);
+            triangles.push_back(openvdb::Vec3I(face[0], face[1], face[2]));
+        }
+        return openvdb::tools::meshToLevelSet<openvdb::FloatGrid>(openvdb::math::Transform{}, inobj.vertices, triangles);
+    }
 }
     
 int main(int argc, const char *argv[])
 {
-    openvdb::initialize();
-    assert(argc == 3);
-    std::string inname(argv[1]);
-    std::ifstream infile(inname);
-    assert(infile);
-    obj inobj;
-    infile >> inobj;
-    std::vector<openvdb::Vec3I> triangles;
-    for(auto const &face: inobj.faces)
+    if(argc != 4)
     {
-        assert(face.size() == 3);
-        triangles.push_back(openvdb::Vec3I(face[0], face[1], face[2]));
+        std::cerr << "usage: " << argv[0] << " inA inB out" << std::endl;
+        return 1;
     }
-    auto grid = openvdb::tools::meshToLevelSet<openvdb::FloatGrid>(openvdb::math::Transform{}, inobj.vertices, triangles);
+    openvdb::initialize();
+    openvdb::FloatGrid::Ptr grid;
+    {
+        std::ifstream infileA(argv[1]);
+        grid = loadMesh(infileA);
+        std::ifstream infileB(argv[2]);
+        auto inB = loadMesh(infileB);
+        openvdb::tools::csgUnion(*grid, *inB);
+    }
+    
     obj outobj;
     std::vector<openvdb::Vec3I> tris;
     std::vector<openvdb::Vec4I> quads;
@@ -180,6 +196,7 @@ int main(int argc, const char *argv[])
             face.push_back(quad[i]);
         outobj.faces.push_back(std::move(face));
     }
-    std::ofstream out(argv[2]);
+    std::ofstream out(argv[3]);
     out << outobj;
+    return 0;
 }
