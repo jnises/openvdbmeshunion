843ef8cd23cafe2489c87950a3c68735e2ab2ef4
Joel Nises <joel@nises.se>
2016-10-30 17:29:25 +0100

    added command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,11 @@
 #include <iostream>
 #include <fstream>
 #include "objmodel.h"
+#include <boost/program_options.hpp>
 
 using namespace deepness::objmodel;
+namespace po = boost::program_options;
+using namespace std;
 
 namespace
 {
@@ -29,21 +32,59 @@ namespace
 
 int main(int argc, const char *argv[])
 {
-    if(argc != 4)
+    float resolution;
+    po::options_description desc("options");
+    desc.add_options()
+    ("help", "produce help message")
+    ("resolution", po::value<float>(&resolution)->default_value(1), "spatial resolution")
+    ("output,O", po::value<string>())
+    ("input,I", po::value<vector<string>>())
+    ;
+    po::positional_options_description p;
+    p.add("input", -1);
+    
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+    po::notify(vm);
+    
+    if (vm.count("help"))
     {
-        std::cerr << "usage: " << argv[0] << " inA inB out" << std::endl;
+        cerr << desc << "\n";
         return 1;
     }
+    
+    string output;
+    vector<string> input;
+    if(vm.count("output"))
+    {
+        output = vm["output"].as<string>();
+    }
+    else
+    {
+        cerr << "output required" << endl;
+        return 1;
+    }
+    
+    if(vm.count("input"))
+    {
+        input = vm["input"].as<vector<string>>();
+    }
+    if(input.size() != 2)
+    {
+        cerr << "2 input files required" << endl;
+        return 1;
+    }
+    
     openvdb::initialize();
     openvdb::FloatGrid::Ptr grid;
     {
-        std::ifstream infileA(argv[1]);
+        std::ifstream infileA(input[0]);
         grid = loadMesh(infileA);
-        std::ifstream infileB(argv[2]);
+        std::ifstream infileB(input[1]);
         auto inB = loadMesh(infileB);
         openvdb::tools::csgUnion(*grid, *inB);
     }
-
+    
     obj outobj;
     std::vector<openvdb::Vec3I> tris;
     std::vector<openvdb::Vec4I> quads;
@@ -63,7 +104,7 @@ int main(int argc, const char *argv[])
             face.push_back(quad[i]);
         outobj.faces.push_back(std::move(face));
     }
-    std::ofstream out(argv[3]);
+    std::ofstream out(output);
     out << outobj;
     return 0;
 }
